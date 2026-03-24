44d52a35b5e522fc9514cc8d6939a3521b08e3ce
Dna9a <youssefabied12@gmail.com>
2026-03-24 03:47:13 +0100

    python

---
+++ module00/ex6/ft_garden_summary.py
@@ -0,0 +1,7 @@
+def ft_garden_summary():
+	garden_name = str(input("Enter garden name: "))
+	plants_numb = int(input("Enter number of plants: "))
+	print("Garden: {}".format(garden_name))
+	print("Plants: {}".format(plants_numb))
+	print("Status: Growing well!")
+
---
+++ module00/ex7/ft_seed_inventory.py
@@ -0,0 +1,12 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) ->None:
+	if (str == "tomato")
+		tomato_seed = str(input("Tomato seeds: "))
+		print("{}  packets available")
+	if (str == "carrot")
+		print("{}  packets available")
+		tomato_seed = str(input("Carrot seeds: "))
+	if (str == "lettuce")
+		tomato_seed = str(input("Lettuce seeds: "))	
+		print("{}  packets available")
+	
+	Tomato seeds:
