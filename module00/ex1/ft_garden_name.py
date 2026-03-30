009b5d3a3d1ec5e3e21f16212c9036f8e64e4d60
YoussefYabied <youssefabied12@gmail.com>
2026-03-30 19:25:32 +0100

    sub + flake8

--- module00/ex1/ft_garden_name.py
+++ module00/ex1/ft_garden_name.py
@@ -1,5 +1,4 @@
 def ft_garden_name():
-	garden_name = input("Enter garden name: ")
-	print("Your garden's name is:", garden_name)
-	print("Status: Growing well!")
-	
+    garden_name = input("Enter garden name: ")
+    print("Your garden's name is:", garden_name)
+    print("Status: Growing well!")
--- module00/ex2/ft_plot_area.py
+++ module00/ex2/ft_plot_area.py
@@ -1,5 +1,5 @@
 def ft_plot_area():
-	length = float(input("Enter the length: "))
-	width = float(input("Enter the width: "))
-	area = length * width
-	print("Plot area:", area)
+    length = float(input("Enter the length: "))
+    width = float(input("Enter the width: "))
+    area = length * width
+    print("Plot area:", area)
