8504837ed371d956d1e2263686aaf1ee3e3de8c7
YoussefYabied <youssefabied12@gmail.com>
2026-03-30 19:01:58 +0100

    push

--- module00/ex7/ft_seed_inventory.py
+++ module00/ex7/ft_seed_inventory.py
@@ -1,11 +1,11 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) ->None:
-	if (str == "tomato")
+	if (str == "tomato"):
 		tomato_seed = str(input("Tomato seeds: "))
 		print("{}  packets available")
-	if (str == "carrot")
+	if (str == "carrot"):
 		print("{}  packets available")
 		tomato_seed = str(input("Carrot seeds: "))
-	if (str == "lettuce")
+	if (str == "lettuce"):
 		tomato_seed = str(input("Lettuce seeds: "))	
 		print("{}  packets available")
 	
