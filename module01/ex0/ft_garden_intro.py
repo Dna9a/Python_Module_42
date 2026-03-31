e251732f6bba83bfd55cf34cea082999fb96a4a9
Dna9a <youssefabied12@gmail.com>
2026-03-31 04:30:03 +0100

    modude01

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,6 @@
+name = str(input("Enter the name of your plant: "))
+height = int(input("Enter the height of your plant (in cm): "))
+age = int(input("Enter the age of your plant (in years): "))
+
+if __name__ == "__main__":
+	print("=== Welcome to My Garden ===")
