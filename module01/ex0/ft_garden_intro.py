3a51b778dd16150bc26a3ab6759168ca15f57990
Dna9a <youssefabied12@gmail.com>
2026-04-03 00:19:39 +0100

    shebang

--- module01/ex0/ft_garden_intro.py
+++ module01/ex0/ft_garden_intro.py
@@ -1,14 +1,12 @@
-def get_plant_info():
-	name = "Rose"
-	height = "25cm"
-	age = "30 days"
-	return name, height, age
+#!/Users/yourname/.pyenv/versions/3.11.9/bin/python
+
+name = "Rose"
+height = "25cm"
+age = "30 days"
 
 if __name__ == "__main__":
 	print("=== Welcome to My Garden ===")
-	name, height, age = get_plant_info()
 	print(f"Name: {name}")
 	print(f"Height: {height}")
-	print(f"Age: {age}")
-	print()
+	print(f"Age: {age} \n")
 	print("=== End of Program ===")
