20486b77ebc046afacc18280b23c22a479d1d67e
YoussefYabied <youssefabied12@gmail.com>
2026-04-03 12:16:09 +0100

    nameissue

---
+++ module01/ex2/ft_plant_growth.py
@@ -0,0 +1,36 @@
+#!/Users/yoabied/.pyenv/versions/3.11.9/bin/python
+
+
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.__age = age
+
+    def show(self) -> None:
+        print(f"{self.name}: {self.height:.1f}cm, {self.__age} days old")
+
+    def grow(self) -> None:
+        self.height += 0.8
+
+    def age(self) -> None:
+        self.__age += 1
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Growth ===")
+
+    rose = Plant(
+        "Rose",
+        24.2,
+        29
+    )
+
+    initial_height: float = rose.height
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        rose.grow()
+        rose.age()
+        rose.show()
+    total_growth: float = round(rose.height - initial_height)
+    print(f"Growth this week: {total_growth}cm")
