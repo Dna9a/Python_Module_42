88f85d1ee90578539bb857727f74224d09dac65f
YoussefYabied <youssefabied12@gmail.com>
2026-04-03 13:15:18 +0100

    commt

--- module01/ex3/ft_plant_factory.py
+++ module01/ex3/ft_plant_factory.py
@@ -10,7 +10,19 @@ class Plant:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
+if __name__ == "__main__":
+    print("=== Garden Plant Registry ===")
 
+    rose = Plant("Rose", 25, 30)
+    oal = Plant("Sunflower", 80, 45)
+    cactus = Plant("Cactus", 15, 120)
+    sunflower = Plant("Sunflowe", 15, 120)
+    fern = Plant("Fern", 15, 120)
+
+    rose.show()
+    sunflower.show()
+    cactus.show()
+    
 
 
 
