c2de8feea2cf5a4c55b49efd2dee17fce7a7c310
YoussefYabied <youssefabied12@gmail.com>
2026-04-14 00:14:43 +0100

    push

---
+++ module06/elements.py
@@ -0,0 +1,7 @@
+# elements.py will contain:
+# •A function create_fire() that just returns the string “Fire element created”.
+# •A function create_water() that just returns the string “Water element created”.
+
+
+def create_fire()
+def create_water()
